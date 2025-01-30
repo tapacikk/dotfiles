9307e8218d22381ddb88458c23b985c35334853b
tapacikk <xbopoctt@gmail.com>
2025-01-30 10:44:31 -0800

    I had to merge tscripts with this repo otherwise doenst make sense

---
+++ dot-local/bin/offlineimap.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+from subprocess import check_output
+import subprocess
+import sys
+sys.path.append('/home/taras/.local/bin')
+
+def get_pass(account):
+    return check_output("pass " + account, shell=True).splitlines()[0]
+
+def get_token(email_address):
+    return subprocess.run(["oama", "access", email_address], capture_output=True, text=True).stdout

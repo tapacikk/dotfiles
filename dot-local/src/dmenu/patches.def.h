f198a88bbdb073189587ed0f0479ed44b759a7cd
tapacikk <xbopoctt@gmail.com>
2025-08-13 14:41:52 -0700

    dmenu

--- dot-local/src/dmenu/patches.def.h
+++ dot-local/src/dmenu/patches.def.h
@@ -5,7 +5,7 @@
  * when including this patch.
  * https://github.com/bakkeby/patches/blob/master/dmenu/dmenu-alpha-5.0_20210725_523aa08.diff
  */
-#define ALPHA_PATCH 1
+#define ALPHA_PATCH 0
 
 /* This adds padding for dmenu in similar fashion to the similarly named patch for dwm. The idea
  * is to have dmenu appear on top of the bar when using said patch in dwm.
@@ -178,7 +178,7 @@
  *
  * https://tools.suckless.org/dmenu/patches/navhistory/
  */
-#define NAVHISTORY_PATCH 1
+#define NAVHISTORY_PATCH 0
 
 /* This patch adds back in the workaround for a BadLength error in the Xft library when color
  * glyphs are used. This is for systems that do not have an updated version of the Xft library

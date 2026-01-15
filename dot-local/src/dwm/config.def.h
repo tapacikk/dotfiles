1efbb5d8347337d953fa05c5ee64a7ea077666fa
tapacikk <xbopoctt@gmail.com>
2026-01-15 09:51:29 -0800

    tweak

--- dot-local/src/dwm/config.def.h
+++ dot-local/src/dwm/config.def.h
@@ -147,7 +147,7 @@ static char *colors[][ColCount] = {
  */
 static char *tagicons[][NUMTAGS] =
 {
-	[DEFAULT_TAGS]        = {"", "", "󰄻", "4", "󰏆", "", "7", "8", "9"},
+	[DEFAULT_TAGS]        = {"", "", "󰄻", "", "󰏆", "", "7", "8", "9"},
 	[ALTERNATIVE_TAGS]    = { "A", "B", "C", "D", "E", "F", "G", "H", "I" },
 	[ALT_TAGS_DECORATION] = { "<1>", "<2>", "<3>", "<4>", "<5>", "<6>", "<7>", "<8>", "<9>" },
 };
